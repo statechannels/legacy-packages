d77d34395d1d9bef05c037f09442cd9b1dbd8b9d
Liam Horne <liam@lihorne.com>
2019-09-23 18:24:07 +0900

    Rename new-packages to packages

--- new-packages/nitro-protocol/contracts/Token.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.5.11;
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
-
-contract Token is ERC20 {
-    constructor() public {
-        _mint(msg.sender, 10000);
-    }
-}
