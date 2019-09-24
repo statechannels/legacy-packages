b7269ff1d3e135f6d199d9adc479fdb55e8cfbb8
Liam Horne <liam@lihorne.com>
2019-09-24 10:46:42 +0900

    Rename new-packages to packages

--- new-packages/nitro-protocol/src/contract/challenge.ts
+++
@@ -1,9 +0,0 @@
-import {Bytes32} from './types';
-import {keccak256, defaultAbiCoder} from 'ethers/utils';
-import {State, hashState} from './state';
-
-export function hashChallengeMessage(challengeState: State): Bytes32 {
-  return keccak256(
-    defaultAbiCoder.encode(['bytes32', 'string'], [hashState(challengeState), 'forceMove']),
-  );
-}
